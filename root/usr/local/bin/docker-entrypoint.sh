55dbf681a78be2b3fe441eb97081b8ecbe99bbac
Christian Bernard <cmbernard333@gmail.com>
2020-07-27 07:42:59 -0700

    More variable updates.

--- root/usr/local/bin/docker-entrypoint.sh
+++ root/usr/local/bin/docker-entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-set -e
+set -Eeuo pipefail
 
 NODE="/usr/bin/node"
 FOUNDRYVTT_HOME=${FOUNDRYVTT_HOME:/home/$FOUNDRYVTT_USER}
+FOUNDRYVTT_BIN=${FOUNDRYVTT_HOME}/foundrybin
+FOUNDRYVTT_DATA=${FOUNDRYVTT_HOME}/foundrydata
 FOUNDRYVTT_URL=${FOUNDRYVTT_URL:""}
-FOUNDRYVTT_OPTS=${FOUNDRYVTT_OPTS} ./resources/app/main.js --dataPath=$FOUNDRYVTT_HOME/foundrydata
+FOUNDRYVTT_OPTS=${FOUNDRYVTT_OPTS} ./resources/app/main.js --dataPath=${FOUNDRYVTT_DATA}
 
 # check for URL
 test -n "${FOUNDRYVTT_URL}" || (echo "FOUNDRYVTT_URL not set" && false)
@@ -13,11 +15,10 @@ test -n "${FOUNDRYVTT_URL}" || (echo "FOUNDRYVTT_URL not set" && false)
 rm -f $FOUNDRYVTT_HOME/foundryvtt.zip
 
 # TODO: we want foundrydata to be mountable
-mkdir -p $FOUNDRYVTT_HOME/foundrydata $FOUNDRYVTT_HOME/foundrybin
+mkdir -p $FOUNDRYVTT_DATA $FOUNDRYVTT_BIN
 wget -O $FOUNDRYVTT_HOME/foundryvtt.zip "$FOUNDRYVTT_URL"
-unzip -d $FOUNDRYVTT_HOME/foundrybin $FOUNDRYVTT_HOME/foundryvtt.zip
+unzip -d $FOUNDRYVTT_BIN $FOUNDRYVTT_HOME/foundryvtt.zip
 
-# CMD["node resources/app/main.js --dataPath=/home/foundryvtt/foundrydata"]
-cd $FOUNDRYVTT_HOME/foundrybin
+cd $FOUNDRYVTT_BIN
 exec 0<&-
 exec ${NODE} ${FOUNDRYVTT_OPTS}
